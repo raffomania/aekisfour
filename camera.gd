04ff35eb33ab005dfef4f752d1da53e9d07f7b57
Rafael Epplée <hello@rafa.ee>
2021-08-23 15:26:39 +0200

    auto-commit

---
+++ camera.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _ready():
+	yield(get_tree().root, "ready")
+	yield(get_tree(), 'idle_frame')
+	var planets = get_tree().get_nodes_in_group('planets')
+	var extents = Rect2()
+	extents.position = planets[0].position
+	for planet in planets:
+		extents = extents.expand(planet.position)
+	var screen_size = Vector2(1920, 1080)
+	var offset = extents.position
+	offset -= screen_size / 2
+	offset += extents.size / 2
+	get_viewport().canvas_transform = Transform2D().translated(-offset)
+
--- planet.gd
+++ planet.gd
@@ -1,19 +1,32 @@
 extends Node2D
 
 var resources = 0
+var selected = true
+const radius = 10
 
 func _ready():
-	var timer = Timer.new()
-	timer.wait_time = 3
-	timer.connect('timeout', self, 'add_ship')
-	add_child(timer)
-	timer.start()
-
-func _process(dt):
-	resources += dt
+	var resource_timer = Timer.new()
+	resource_timer.wait_time = 5
+	resource_timer.connect('timeout', self, 'tick_resources')
+	add_child(resource_timer)
+	resource_timer.start()
+	add_to_group('planets')
 
 func _draw():
-	draw_circle(Vector2.ZERO, 10, Color.white)
+	draw_circle(Vector2.ZERO, radius, Color.cyan)
+	for i in range(resources):
+		var position = Vector2(radius + 5, 0).rotated(i * TAU / 20)
+		draw_circle(position, 2, Color.beige)
+	if selected:
+		var offset = radius + 10 
+		draw_line(Vector2(offset, offset), Vector2(offset, offset / 2), Color.white)
+		draw_line(Vector2(offset, offset), Vector2(0, offset), Color.white)
+		draw_line(Vector2(-offset, -offset), Vector2(-offset, -offset / 2), Color.white)
+		draw_line(Vector2(-offset, -offset), Vector2(0, -offset), Color.white)
 
-func add_ship(): 
-	$'../ships'.add_ship()
+func _process(_delta):
+	update()
+
+func tick_resources():
+	resources += 1
+	update()
